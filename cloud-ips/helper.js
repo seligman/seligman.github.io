397fa8c5d98261b0efb26f70ba83194dcd527cc2
Scott Seligman <scott.seligman@gmail.com>
2022-09-14 14:34:48 -0700

    Add the range back

--- cloud-ips/helper.js
+++ cloud-ips/helper.js
@@ -5,6 +5,26 @@
 var Address4 = require('ip-address').Address4;
 var Address6 = require('ip-address').Address6;
 
+ipToString = function(ip, v6) {
+    if (v6) {
+        ip = ip.address;
+        ip = ip.replace(/\b:?(?:0+:?){2,}/, '::');
+        return ip.split(':').map(x => x.replace(/^0+(.)$/, '$1')).join(':');
+    } else {
+        return ip.address;
+    }
+}
+
+cidrToIPs = function(cidr) {
+    if (cidr.includes(":")) {
+        temp = new Address6(cidr);
+        return [ipToString(temp.startAddress(), true), ipToString(temp.endAddress(), true)];
+    } else {
+        temp = new Address4(cidr);
+        return [ipToString(temp.startAddress(), false), ipToString(temp.endAddress(), false)];
+    }
+}
+
 ipToBits = function(ip) {
     if (ip.includes(":")) {
         return new Address6(ip).getBitsBase2();
