6163d497ca5d831adcca36fe2661dcb4037d98ab
Scott Seligman <scott.seligman@gmail.com>
2022-09-14 14:20:25 -0700

    Use the database

--- cloud-ips/helper.js
+++ cloud-ips/helper.js
@@ -1,50 +1,14 @@
 /* 
-    To build this, use ./build_helper.js
+    To build this, use ./build_helper.py
 */
 
 var Address4 = require('ip-address').Address4;
 var Address6 = require('ip-address').Address6;
 
-crackCidr = function(cidr) {
-    if (cidr.includes(":")) {
-        temp = new Address6(cidr);
+ipToBits = function(ip) {
+    if (ip.includes(":")) {
+        return new Address6(ip).getBitsBase2();
     } else {
-        temp = new Address4(cidr);
-    }
-    return [temp.startAddress().bigInteger(), temp.endAddress().bigInteger(), cidr];
-}
-
-ipToString = function(ip, v6) {
-    if (v6) {
-        ip = Address6.fromBigInteger(ip).address;
-        ip = ip.replace(/\b:?(?:0+:?){2,}/, '::');
-        return ip.split(':').map(x => x.replace(/^0+(.)$/, '$1')).join(':');
-    } else {
-        return Address4.fromBigInteger(ip).address;
-    }
-}
-
-checkCidr = function(ip, callback, callbackIn) {
-    try {
-        var v6;
-        if (ip.includes(":")) {
-            ip = new Address6(ip).bigInteger();
-            v6 = true;
-        } else {
-            ip = new Address4(ip).bigInteger();
-            v6 = false;
-        }
-        for (var i = 0; ; i++) {
-            var cidr = callback(i, v6);
-            if (cidr === null) {
-                break;
-            }
-            if (ip.compareTo(cidr[0]) >= 0 && ip.compareTo(cidr[1]) <= 0) {
-                callbackIn(i, v6);
-            }
-        }
-        return true;
-    } catch (error) {
-        return false;
+        return new Address4(ip).getBitsBase2();
     }
 }
